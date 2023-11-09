396f3da18632ff0746f0eac24b9fcead4280305a
Lukas Vecerka <vecerkalukas18@gmail.com>
2023-11-09 18:46:18 +0100

    implemented simple communication between server and client

--- src/client/tftp_client.cpp
+++ src/client/tftp_client.cpp
@@ -1,6 +1,12 @@
 // tftp_server.cpp
 #include "client/tftp_client.hpp"
 #include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
 
 TFTPClient::TFTPClient(std::string hostname, int port)
     : hostname(std::move(hostname)), port(port) {}
@@ -9,9 +15,47 @@ void TFTPClient::upload(std::string dest_filepath) {
     std::cout << "Uploading file to " << hostname << ":" << port 
               << " with destination filepath: " << dest_filepath << std::endl;
 
-    // Code to bind the socket and listen for incoming requests...
+        // Create socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        std::cerr << "Cannot open socket\n";
+        return;
+    }
 
-    // ... and then entering the main loop to handle those requests.
+    // Initialize hints for getaddrinfo
+    struct addrinfo hints, *res;
+    std::memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;      // AF_INET for IPv4
+    hints.ai_socktype = SOCK_DGRAM; // Datagram socket for UDP
+
+    // Resolve hostname to IP address
+    if (getaddrinfo(hostname.c_str(), nullptr, &hints, &res) != 0) {
+        std::cerr << "Could not resolve hostname\n";
+        close(sockfd);
+        return;
+    }
+
+    // Copy the resolved address to our server address structure
+    struct sockaddr_in server_addr = *(struct sockaddr_in*)res->ai_addr;
+    server_addr.sin_port = htons(port);
+
+    // Prepare data to be sent
+    const char* message = "Hello TFTP Server!";
+    size_t message_length = std::strlen(message);
+
+    // Send data to server
+    if (sendto(sockfd, message, message_length, 0, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        std::cerr << "Sendto failed\n";
+        close(sockfd);
+        return;
+    }
+
+    std::cout << "Message sent to server successfully\n";
+
+    // Here you might want to wait for a response depending on your application's needs
+
+    // Close the socket
+    close(sockfd);
 }
 
 void TFTPClient::download(std::string filepath, std::string dest_filepath) {
--- src/server/tftp_server.cpp
+++ src/server/tftp_server.cpp
@@ -1,6 +1,11 @@
 // tftp_server.cpp
 #include "server/tftp_server.hpp"
 #include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 TFTPServer::TFTPServer(int port, std::string rootDirPath)
     : port(port), rootDirPath(std::move(rootDirPath)) {}
@@ -9,7 +14,51 @@ void TFTPServer::start() {
     std::cout << "Starting TFTP server on port " << port 
               << " with root directory: " << rootDirPath << std::endl;
 
-    // Code to bind the socket and listen for incoming requests...
+        // Create socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        std::cerr << "Cannot open socket\n";
+        return;
+    }
 
-    // ... and then entering the main loop to handle those requests.
+    // Initialize server address structure
+    struct sockaddr_in server_addr;
+    std::memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // Listen on all interfaces
+    server_addr.sin_port = htons(port);
+
+    // Bind socket to the server address
+    if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        std::cerr << "Cannot bind socket to port " << port << "\n";
+        close(sockfd);
+        return;
+    }
+
+    std::cout << "Server listening on port " << port << std::endl;
+
+    // Enter a loop to receive data
+    while (true) {
+        struct sockaddr_in client_addr;
+        socklen_t client_len = sizeof(client_addr);
+        char buffer[2048]; // A buffer to store incoming data
+
+        // Receive data from a client
+        ssize_t received_bytes = recvfrom(sockfd, buffer, sizeof(buffer), 0,
+                                          (struct sockaddr *)&client_addr, &client_len);
+        if (received_bytes < 0) {
+            std::cerr << "Failed to receive data\n";
+            continue; // In a real server you might want to handle this differently
+        }
+
+        // Print the received data to stdout for demonstration purposes
+        std::cout << "Received data: " << std::string(buffer, received_bytes) << std::endl;
+
+        // Here you would normally process the received data and respond accordingly
+        // For now, just echo the data back to the sender for testing purposes
+        sendto(sockfd, buffer, received_bytes, 0, (struct sockaddr *)&client_addr, client_len);
+    }
+
+    // Close the socket (this will never be reached in this example, but should be done when the server stops)
+    close(sockfd);
 }
